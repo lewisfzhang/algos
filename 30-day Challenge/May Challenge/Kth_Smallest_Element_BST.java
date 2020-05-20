4061c72d093cf4b77d79cfdb6f317cc8a64177ee
Lewis Zhang <lzhang19@berkeley.edu>
2020-05-20 10:59:54 -0700

    may 20

---
+++ 30-day Challenge/May Challenge/Kth_Smallest_Element_BST.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int val, n;
+
+    public int kthSmallest(TreeNode root, int k) {
+        n = k;
+        iterator(root);
+        return val;
+    }
+
+    public void iterator(TreeNode root) {
+        if (root == null) return;
+
+        if (root.left != null) iterator(root.left);
+
+        if (--n == 0) {
+            val = root.val;
+            return;
+        }
+
+        if (root.right != null) iterator(root.right);
+    }
+}
